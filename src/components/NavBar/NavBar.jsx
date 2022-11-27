ce4c4893a979ebe876004bdf3d69ed657a63308c
Franco <juanfrancorodriguez99@gmail.com>
2022-11-27 02:29:33 -0300

    small fix on cart navbar, it was an array of values when total its a signel value

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -83,15 +83,8 @@ export default function NavBar() {
       <div className={styles.cartFooterContainer}>
         <div className={styles.subtotalContainer}>
           <h3>Subtotal:</h3>
-          {
-            cartItems.map(c => {
-              let subtotal =+ Number(c.price) * Number(c.quantity)
-
-              return (
-                <span key={c.id} >${subtotal}</span>
-              )
-            })
-          }
+		<span>{cartItems.reduce((subtotal, c) => subtotal + Number(c.price) * Number(c.quantity), 0)}</span>
+          
         </div>
         <Button
           variant="outlined"
@@ -203,3 +196,4 @@ export default function NavBar() {
     </nav>
   );
 }
+
