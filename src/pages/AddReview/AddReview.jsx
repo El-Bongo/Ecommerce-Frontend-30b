bcf26f5a22fdc3f13b3ba4c97adeccd2646df2f2
FedeHerrera <federicodecomercio@gmail.com>
2022-12-10 20:56:18 -0300

    Chatbot y reviews

--- src/pages/AddReview/AddReview.jsx
+++ src/pages/AddReview/AddReview.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from "react-router-dom";
 import './AddReview.css'
+import ReactStars from "react-rating-stars-component";
 import { createReview } from '../../redux/actions';
 import { useNavigate } from "react-router-dom";
 
@@ -25,6 +26,7 @@ import { useNavigate } from "react-router-dom";
 
 
 const AddReview = () => {
+
     let { id } = useParams(); 
     const User = useSelector(state => state.user)
     const { Article } = useSelector((state) => state.details.detailedArticle);
@@ -39,28 +41,6 @@ const AddReview = () => {
       item: id,
 /*       image: '', Por ahora no, debo charlar como lo haremos.
  */    });
-
-
-/*     const loadFile = function(e) {
-      const image = document.getElementById('output');
-      const file = e.target.files[0]
-      reader.addEventListener("load", () => {
-        // convert image file to base64 string
-        image.src = reader.result;
-        setInput({
-          ...input,
-          [e.target.name]: reader.result
-      })
-      }, false);
-      if (file) {
-        reader.readAsDataURL(file);
-      }    
-      setInput({
-        ...input,
-        [e.target.name]: reader.result
-    })
-  } */
-
 
     let handleChange = (e) => {
       e.preventDefault();
@@ -82,92 +62,13 @@ const AddReview = () => {
       navigate()
  
     }
-
-    const stars = [];
-    for (let i = 0; i <= 4; i++) {
-        stars.push({
-        active: false,
-        index: i
-      });
-    }
+    const ratingChanged = (newRating) => {
+      console.log(newRating);
+      input.rating=newRating
+      setErrors(validate(
+        input))
+      };
     
-    const renderStars = (parentElement, stars, activeContent, notActiveContent) => {
-        parentElement.innerHTML = '';
-        stars.forEach(({ active, index }) => {
-        parentElement.innerHTML += `
-            <span class="r__icon">${active ? activeContent : notActiveContent}</span>`;
-      });
-      
-      Array.from(parentElement.querySelectorAll('.r__icon')).forEach((item, itemIndex) => {
-        const star = stars.find(({ index }) => index === itemIndex);
-        stars[star.index].element = item;
-      
-        item.addEventListener('click', (e) => {
-            const itemElement = e.target;
-          const starIndex = stars.findIndex(({ element }) => element === itemElement);
-          if (starIndex === -1) {
-            return;
-          }
-          
-          const toActive = stars[starIndex].active !== true;
-          input.rating = starIndex+1
-          validate(input)    
-          stars = stars.map(star => {
-            if (toActive) {
-                // Set items before index to true, and after to false
-                if (star.index <= starIndex) {
-                return {
-                    ...star,
-                  active: true
-                };
-              }
-              
-              return {
-                ...star,
-                active: false
-              };
-            } else {
-                // Set items after index to false, and before to true
-              if (star.index >= starIndex) {
-                return {
-                  ...star,
-                  active: false
-                };
-              }
-              
-              return {
-                ...star,
-                active: true
-              };
-            }
-          });
-          
-          renderStars(parentElement, stars, activeContent, notActiveContent);
-        });
-      });
-    };
-    
-    const setupStars = (stars, activeContent, notActiveContent) => {
-        const parentElement = document.getElementById("w__stars");
-      if (!parentElement) {
-        return;
-      }
-      
-      renderStars(parentElement, stars, activeContent, notActiveContent);
-    };
-    
-    setupStars(stars, '[X]', '[ ]');
-    
-    const handleRating = (e) => {
-        setInput({
-          ...input,
-          rating: [...input.rating, e.target.value]
-        })
-        setErrors(validate({
-          ...input,
-          rating: [...input.rating, e.target.value]
-        }, Article))
-    }
   return (
     <>
     <div id="background2">
@@ -184,6 +85,15 @@ const AddReview = () => {
           type="text"
           name="description"
           onChange={handleChange}
+
+        />
+    </div>
+    <div className="stars">
+    <ReactStars
+          count={5}
+          onChange={ratingChanged}
+          size={36}
+          activeColor="#ffd700"
         />  
     </div>
     <div >
@@ -195,13 +105,13 @@ const AddReview = () => {
 
 
       </div>
-      <div class="flex flex-row product-star-con" id="w__stars"></div>
-
+       <div class="flex flex-row product-star-con" id="w__stars"></div>
       <button disabled={errors.description || errors.rating || justloaded === true} type="submit" className>Añadir reseña</button>
       { errors.description && (<p className="danger">{errors.description}</p>) }
       { errors.rating && (<p className="danger">{errors.rating}</p>) }
 
       </form>
+
       </div>  
       </div> 
       </>
