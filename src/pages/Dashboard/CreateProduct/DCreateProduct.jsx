2e72ee606540e04ab415658018a5efe786f3cce4
Santiago Segurado <santisegurado1@gmail.com>
2022-12-13 12:13:44 -0300

    Panel de Usuario crear Producto

--- src/pages/Dashboard/CreateProduct/DCreateProduct.jsx
+++ src/pages/Dashboard/CreateProduct/DCreateProduct.jsx
@@ -1,30 +1,41 @@
 import styles from "./DCreateProduct.module.scss";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DSidebar } from "../components/Sidebar/DSidebar";
 import { DNavbar } from "../components/Navbar/DNavbar";
 import dark from "../Dark.module.scss";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { uploadPhotoToCloudinary } from "../../../hooks/uploadToCloudinary";
 import Swal from "sweetalert2";
+import { getAllCategories } from "../../../redux/actions";
 
 export const DCreateProduct = () => {
-  const [avatar, setAvatar] = useState(null);
-  const [productUpdate, setProductUpdate] = useState();
+  const categorias = useSelector((state) => state.articles.categorias);
   const { darkMode } = useSelector((state) => state.darkmode);
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const [avatar, setAvatar] = useState(null);
+  const [productCreate, setProductCreate] = useState({
+    title: "",
+    precio: "",
+    stock: 0,
+    category: {},
+  });
+  const [properties, setProperties] = useState({
+    brand: "",
+    model: "",
+  });
   // const navigate = useNavigate();
 
-  const handleUpdateUser = async (user, images) => {
-    const resp = await fetch(
-      "http://localhost:3001/articulo/modify",
-      {
-        method: "POST",
-        body: JSON.stringify({ ...user, images }),
-        headers: new Headers({ "content-type": "application/json" }),
-      }
-    );
+  useEffect(() => {
+    dispatch(getAllCategories());
+  }, [dispatch]);
+
+  const handleUpdateUser = async (product, images, properties) => {
+    const resp = await fetch("http://localhost:3001/articulo/create", {
+      method: "POST",
+      body: JSON.stringify({ ...product, images, properties }),
+      headers: new Headers({ "content-type": "application/json" }),
+    });
     const data = await resp.json();
 
     console.log(data);
@@ -39,18 +50,19 @@ export const DCreateProduct = () => {
       images = await response();
     }
 
+
     Swal.fire({
-      title: "Quieres aceptar los cambios?",
+      title: "Quieres crear un nuevo producto?",
       showDenyButton: true,
       showCancelButton: true,
-      confirmButtonText: "Editar",
-      denyButtonText: `No Editar`,
+      confirmButtonText: "Crear",
+      denyButtonText: `No Crear`,
     }).then((result) => {
       if (result.isConfirmed) {
-        Swal.fire("Editado!", "", "success");
-        handleUpdateUser(productUpdate, images);
+        Swal.fire("Creado!", "", "success");
+        handleUpdateUser(productCreate, images, properties);
       } else if (result.isDenied) {
-        Swal.fire("Los cambios no se guardaron", "", "info");
+        Swal.fire("El producto no fue creado", "", "info");
       }
     });
   };
@@ -75,6 +87,7 @@ export const DCreateProduct = () => {
             />
           </div>
           <div className={styles.right}>
+            <h3 style={{ padding: 10 }}>Generales</h3>
             <form onSubmit={handleSubmit}>
               <div className={styles.formInput}>
                 <label htmlFor="file">
@@ -96,8 +109,8 @@ export const DCreateProduct = () => {
                   type="text"
                   style={{ color: darkMode && "white" }}
                   onChange={(e) =>
-                    setProductUpdate({
-                      ...productUpdate,
+                    setProductCreate({
+                      ...productCreate,
                       title: e.target.value,
                     })
                   }
@@ -109,9 +122,9 @@ export const DCreateProduct = () => {
                   type="text"
                   style={{ color: darkMode && "white" }}
                   onChange={(e) =>
-                    setProductUpdate({
-                      ...productUpdate,
-                      price: e.target.value,
+                    setProductCreate({
+                      ...productCreate,
+                      precio: e.target.value,
                     })
                   }
                 />
@@ -122,18 +135,91 @@ export const DCreateProduct = () => {
                   type="number"
                   style={{ color: darkMode && "white" }}
                   onChange={(e) =>
-                    setProductUpdate({
-                      ...productUpdate,
+                    setProductCreate({
+                      ...productCreate,
                       stock: e.target.value,
                     })
                   }
                 />
               </div>
-              
-              {!avatar?.target.files.length &&
-              (productUpdate?.title === "" || !productUpdate?.title) &&
-              (productUpdate?.price === "" || !productUpdate?.price) &&
-              (productUpdate?.stock === "" || !productUpdate?.stock) ? (
+              <div className={styles.formInput}>
+                <label>Categorias</label>
+                <select
+                  defaultValue="Categorias"
+                  onChange={(e) =>
+                    setProductCreate({
+                      ...productCreate,
+                      category: {id: e.target.value },
+                    })
+                  }
+                >
+                  <option disabled hidden>
+                    Categorias
+                  </option>
+                  {categorias.map((x) => (
+                    <option key={x.id + x.name} value={x.id}>
+                      {x.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <h3 style={{ padding: 10 }}>Propiedades</h3>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    gap: 40,
+                  }}
+                >
+                  <div className={styles.formInput}>
+                    <label>Marca</label>
+                    <input
+                      type="text"
+                      style={{ color: darkMode && "white" }}
+                      onChange={(e) =>
+                        setProperties({
+                          ...properties,
+                          brand: e.target.value,
+                        })
+                      }
+                    />
+                  </div>
+                  <div className={styles.formInput}>
+                    <label>Modelo</label>
+                    <input
+                      type="text"
+                      style={{ color: darkMode && "white" }}
+                      onChange={(e) =>
+                        setProperties({
+                          ...properties,
+                          model: e.target.value,
+                        })
+                      }
+                    />
+                  </div>
+                </div>
+              </div>
+
+              {!avatar?.target.files.length ||
+              productCreate?.title === "" ||
+              !productCreate?.title ||
+              productCreate?.precio === "" ||
+              !productCreate?.precio ||
+              productCreate?.stock === "" ||
+              !productCreate?.stock ||
+              properties?.brand === "" ||
+              !properties?.brand ||
+              properties?.model === "" ||
+              !properties?.model || !productCreate?.category.id ? (
                 <button disabled style={{ backgroundColor: "#ac96fd" }}>
                   Enviar
                 </button>
