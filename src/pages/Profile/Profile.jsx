10b345e5e31946745d83fdda67c7c2f3d7e84e14
Nicolás Hernán Silva <83423412+El-Bongo@users.noreply.github.com>
2022-12-14 18:02:11 -0300

    Update Profile.jsx
    Era con s  :'v

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -61,8 +61,8 @@ export default function Profile() {
   }
 
   function handleErrorNick(e){
-    if(e.length < 4) return "El nombre es demaciado corto"
-    else if(e.length > 16) return "El nombre es demaciado largo"
+    if(e.length < 4) return "El nombre es demasiado corto"
+    else if(e.length > 16) return "El nombre es demasiado largo"
     else if(!/^[a-zA-Z0-9\s]+$/i.test(e)) return "El nombre tiene caracteres incorrectos"
     else return null
   }
